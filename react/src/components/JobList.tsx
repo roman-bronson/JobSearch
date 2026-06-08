4457d5c2d5a39d967414e1cda92830a0705b0f7d
Roman Bronson <rbronson2004@gmail.com>
2026-06-08 16:37:16 -0400

    created a component that stores and renders the list of jobs

---
+++ react/src/components/JobList.tsx
@@ -0,0 +1,17 @@
+import JobCard from "./JobCard";
+import type { Job } from "../types/Job";
+
+interface JobListProps {
+    jobs: Job[];
+    onDelete: (id: string) => void;
+}
+
+export default function JobList ({ jobs, onDelete }: JobListProps) {
+    return (
+        <>
+            {jobs.map((job) => (
+                <JobCard key={job.id} job={job} onDelete={() => onDelete(job.id)} />
+            ))}
+        </>
+    );
+}
