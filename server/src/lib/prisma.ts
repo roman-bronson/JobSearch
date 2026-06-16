e2042aedb59d8d840168fe0e78f93d20a545b092
Roman Bronson <rbronson2004@gmail.com>
2026-06-15 20:02:27 -0400

    Created prisma instance and client to handle db queries

---
+++ server/src/lib/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "../../generated/prisma/client";
+
+const adapter = new PrismaMariaDb({
+    host: "localhost",
+    port: 3306,
+    connectionLimit: 5,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
