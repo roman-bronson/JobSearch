53a8d851043c5206f712e03c9d138bbe33a6a720
Roman Bronson <rbronson2004@gmail.com>
2026-07-01 19:38:04 -0400

    added empty tage to be able to render everything properly

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -10,11 +10,13 @@ interface HeaderProps {
 export default function Header ( { toggleRenderJobCardForm, addJob, renderAddJobForm }: HeaderProps) {
     console.log(toggleRenderJobCardForm);
     return (
-        <div className="header">
-            <h2>Jobs</h2>
+        <>
+            <div className="header">
+                <h2>Jobs</h2>
 
-            <button type="button" className="btn btn-success" onClick={toggleRenderJobCardForm}>Add a Job</button>
+                <button type="button" className="btn btn-success" onClick={toggleRenderJobCardForm}>Add a Job</button>
+            </div>
             { renderAddJobForm && (<AddJobCard addJob={addJob} toggleRenderJobCardForm={toggleRenderJobCardForm}/>)}
-        </div>
+        </>
     );
 }
